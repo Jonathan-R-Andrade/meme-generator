4a8313810a71390c7cd19e85c6e6162fb935a178
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2021-12-17 13:41:27 -0300

    Requisito 2 feito

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // Elementos da página
 let textInput;
 let memeText;
+let memeInsert;
+let memeImage;
 
 // Pega o texto digitado no input e coloca na imagem
 function pegarTextoDoInput(event) {
   memeText.innerText = textInput.value;
 }
 
+// Obtem a imagem seleciona
+function obterImagem() {
+  // Liberando o objeto da memoria
+  window.URL.revokeObjectURL(memeImage.src);
+  // Criando um objeto URL
+  const objURL = URL.createObjectURL(memeInsert.files[0]);
+  // Adicionado a URL da imagem no elemento img
+  memeImage.src = objURL;
+}
+
 // Pega os elementos da página
 function obterElementos() {
   textInput = document.getElementById('text-input');
   memeText = document.getElementById('meme-text');
+  memeInsert = document.getElementById('meme-insert');
+  memeImage = document.getElementById('meme-image');
 }
 
 // Adiciona ouvintes aos elementos da página
 function adicionarOuvintes() {
   textInput.addEventListener('input', pegarTextoDoInput);
+  memeInsert.addEventListener('change', obterImagem);
 }
 
 // Executa o código após o carregamento da página
